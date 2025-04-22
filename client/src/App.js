663b88515ef981b45e83e6fced2abae4dbace089
unknown <ijihad639@gmail.com>
2025-04-22 18:04:02 +0600

    chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-api-dusky.vercel.app");
+const socket = io("http://localhost:5000");
 
 function App() {
   const [message, setMessage] = useState("");
