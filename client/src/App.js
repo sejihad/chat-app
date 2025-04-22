9b6571a2928321563a82236264bcc5bd868d9105
unknown <ijihad639@gmail.com>
2025-04-22 18:06:53 +0600

    chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-api-vj4l.onrender.com");
 
 function App() {
   const [message, setMessage] = useState("");
